cfe6ee747fdc0b73a79d0233eb9b306096542b9e
KatyaKaratnyk <k.bujgina@gmail.com>
2017-09-07 22:41:09 +0300

    HoweTask2

--- src/com/butterfly/Client.java
+++ src/com/butterfly/Client.java
@@ -1,17 +1,12 @@
 package com.butterfly;
 
 public class Client {
-    private static int id;
-    private int idClient;
+
     private Order order;
 
     public Client (Order order) {
-        ++id;
-        this.idClient = id;
+
         this.order = order;
-    }
-    public int getIdClient() {
-        return idClient;
     }
 
     public Order getOrder() {
